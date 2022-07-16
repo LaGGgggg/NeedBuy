32b710f701ffa401125a7b2e57baafdef060e33c
LaGGgggg <Poni22poni23@yandex.ru>
2022-07-16 10:51:11 +0400

    Added work home page.
    Added index.html, urls (in app_main and in home_page_app), view and main.css.

---
+++ NeedBuy/home_page_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from home_page_app import views  # ignore this import error, all works correct.
+
+urlpatterns = [
+    path('', views.view_index, name='view-index')
+]
--- NeedBuy/home_page_app/views.py
+++ NeedBuy/home_page_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_index(request):
+
+    match request.method:
+
+        case 'GET':
+
+            return render(request, 'home_page_app/index.html')
