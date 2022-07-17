cba7e97211d94931a0c2a9d3b0a68996ec0c4d08
LaGGgggg <Poni22poni23@yandex.ru>
2022-07-17 19:19:17 +0400

    Move home_page_app for best project organization.
    Now it in "apps" directory.

--- NeedBuy/app_main/settings.py
+++ NeedBuy/app_main/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'home_page_app.apps.HomePageAppConfig',
+    'apps.home_page_app.apps.HomePageAppConfig',
 ]
 
 MIDDLEWARE = [
