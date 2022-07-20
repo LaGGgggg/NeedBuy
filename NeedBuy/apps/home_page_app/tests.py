6dbfcb828b07e6f4dde3ed21c7e2ba17103487a7
LaGGgggg <Poni22poni23@yandex.ru>
2022-07-20 15:38:20 +0400

    Added home_page_app test.
    Added home_page_app test.
    Changed py --> python in procfile command.

--- NeedBuy/apps/home_page_app/tests.py
+++ NeedBuy/apps/home_page_app/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.test.client import Client
 
 # Create your tests here.
+
+
+class HomePageAppTestCase(TestCase):
+
+    def setUp(self):
+
+        self.client = Client()
+
+    def test_main_page_requests(self):
+
+        # index.html
+
+        request = self.client.get('/')
+
+        self.assertTemplateUsed(request, 'home_page_app/index.html')
+        self.assertEqual(request, 200)
+
+        # error.html
+
+        request = self.client.get('/bad-address/')
+
+        self.assertTemplateUsed(request, 'error.html')
+        self.assertEqual(request, 200)
