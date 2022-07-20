8952c4fed04d36ef2896761d003d97475cd08319
LaGGgggg <Poni22poni23@yandex.ru>
2022-07-20 17:49:46 +0400

    Update settings.py

--- NeedBuy/app_main/settings.py
+++ NeedBuy/app_main/settings.py
@@ -92,9 +92,22 @@ WSGI_APPLICATION = 'app_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(),
-}
+if os.environ.get('GITHUB_WORKFLOW'):
+    # It for github autotests
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(),
+    }
 
 django_heroku.settings(locals())
 
