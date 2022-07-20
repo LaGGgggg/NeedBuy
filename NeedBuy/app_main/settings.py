cd5ccf1c6267adc24fe9e56f6b5b509a9ad93849
LaGGgggg <Poni22poni23@yandex.ru>
2022-07-20 18:04:13 +0400

    Update settings.py

--- NeedBuy/app_main/settings.py
+++ NeedBuy/app_main/settings.py
@@ -100,7 +100,7 @@ if os.environ.get('GITHUB_WORKFLOW'):
            'NAME': 'github_actions',
            'USER': 'postgres',
            'PASSWORD': 'postgres',
-           'HOST': 'postgres',
+           'HOST': '127.0.0.1',
            'PORT': '5432',
         }
     }
